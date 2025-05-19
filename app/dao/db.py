f13851a63fdda0d142c52393d94b441caaf0a52d
GABRIEL <negoucb1@gmail.com>
2025-05-18 22:55:26 -0300

    Update db.py

--- app/dao/db.py
+++ app/dao/db.py
@@ -1,1 +1,14 @@
+import MySQLdb
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g:
+        g.db = MySQLdb.connect(
+            host=current_app.config['DB_HOST'],
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASSWORD'],
+            database=current_app.config['DB_NAME'],
+            cursorclass=MySQLdb.cursors.DictCursor
+        )
+    return g.db
 
